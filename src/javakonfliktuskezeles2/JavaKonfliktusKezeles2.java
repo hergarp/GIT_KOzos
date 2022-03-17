439ce87959fb6badbcd48317ba5212711f814492
Bojhos <bojtos.mate.denes@diak.szamalk-szalezi.hu>
2022-03-17 18:03:36 +0100

    4. név inicializálása

--- src/javakonfliktuskezeles2/JavaKonfliktusKezeles2.java
+++ src/javakonfliktuskezeles2/JavaKonfliktusKezeles2.java
@@ -5,16 +5,10 @@ package javakonfliktuskezeles2;
 public class JavaKonfliktusKezeles2 {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         String nev1, 
         String nev2, 
         String nev3, 
         String nev4 = "HP" 
-                
-=======
-        String nev1, nev2, nev3,nev4;
-
->>>>>>> aeb6857c27acd20fb3d973c6d11dcb2d15e8eb77
-    }
-    
+                    
 }
